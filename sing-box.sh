95518b601febeb82055557977e7b1a4dcba65b7b
webwww74 <115360573+webwww74@users.noreply.github.com>
2025-10-23 09:34:47 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -440,7 +440,7 @@ After=network.target
 Type=simple
 NoNewPrivileges=yes
 TimeoutStartSec=0
-ExecStart=/etc/sing-box/argo tunnel --url http://localhost:8001 --no-autoupdate --edge-ip-version auto --protocol http2 > /etc/sing-box/argo.log 2>&1
+ExecStart=/etc/sing-box/argo tunnel --url http://localhost:8001 --no-autoupdate --edge-ip-version auto --protocol http2 > /etc/sing-box/argo.log
 Restart=on-failure
 RestartSec=5s
 
@@ -477,7 +477,7 @@ EOF
 #!/sbin/openrc-run
 
 description="Cloudflare Tunnel"
-command="/etc/sing-box/argo tunnel --url http://localhost:8001 --no-autoupdate --edge-ip-version auto --protocol http2 > /etc/sing-box/argo.log 2>&1"
+command="/etc/sing-box/argo tunnel --url http://localhost:8001 --no-autoupdate --edge-ip-version auto --protocol http2 > /etc/sing-box/argo.log"
 #!/command_args="-c '/etc/sing-box/argo tunnel --url http://localhost:8001 --no-autoupdate --edge-ip-version auto --protocol http2 > /etc/sing-box/argo.log 2>&1'"
 command_background=true
 pidfile="/var/run/argo.pid"
